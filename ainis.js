33ceb89671642780d64e242e41a7a29ec7001c4d
chiurly <chiurly@gmail.com>
2022-12-05 21:59:27 +0200

    Update ainis.js

--- ainis.js
+++ ainis.js
@@ -1,40 +1,45 @@
 const { updateUser, createUser } = require('./api/users')
 
 function saveUser(user) {
-    const errors = []
-    if (user.username) {
-        if (user.username.length < 3) {
-            errors.push("Username must be 3 or more characters")
-        }
-    } else {
-        errors.push("Username is required")
-    }
+  if (user.id == null) {
+    console.log('Created User')
+    createUser(user)
+  } else {
+    console.log('Updated User')
+    updateUser(user)
+  }
+}
 
-    if (user.password) {
-        if (user.password.length < 8) {
-            errors.push("Password must be 8 or more characters")
-        }
-    } else {
-        errors.push("Password is required")
-    }
+function validateUser(user) {
+  return [
+    ...validateUsername(user.username),
+    ...validatePassword(user.password)
+  ]
+}
 
-    if (errors.length > 0) {
-        errors.forEach(error => console.error(error))
-        return
-    }
+function validateUsername(username) {
+  const errors = []
+  if (!username) errors.push('Username is required')
+  if (username != null && username.length < 3) errors.push('Username must be 3 or more characters')
+  return errors
+}
 
-    if (user.id == null) {
-        console.log('Created User')
-        createUser(user)
-    } else {
-        console.log('Updated User')
-        updateUser(user)
-    }
+function validatePassword(password) {
+  const errors = []
+  if (!password) errors.push('Password is required')
+  if (password != null && password.length < 8) errors.push('Password must be 8 or more characters')
+  return errors
 }
 
 const user = {
-    username: '',
-    password: 'password'
+  username: 'WDS',
+  password: 'password'
+}
+
+const errors = validateUser(user)
+if (errors.length > 0) {
+  errors.forEach(error => console.error(error))
+  return
 }
 
 saveUser(user)
